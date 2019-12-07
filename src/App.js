d6f042645bad33f5ebea4d6ae5a54c4158b6e3c9
kangxiaojiang <952066037@qq.com>
2019-12-07 13:36:34 +0800

    init app

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import React,{Component} from 'react'
+import {Button,message} from 'antd'
+
+export default class App extends Component{
+
+    render (){
+        return <Button type="primary">Primary</Button>
+    }
+}
